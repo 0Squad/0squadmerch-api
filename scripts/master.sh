6aab80fd65a1c4944a1e5b8d6016e290aec4cec9
laurpaik <laurpaik@gmail.com>
2017-03-08 14:14:00 -0500

    Edit master script for Lauren's machine
    - needed to test with seed data

--- scripts/master.sh
+++ scripts/master.sh
@@ -3,10 +3,10 @@
 API="http://localhost:4741"
 URL_PATH="/products"
 # ID="58ba357cf61cbefc4823f8ac"
-TOKEN="qklS1KkOTHP8VxjJfJNtNv5fgEQHxEkoxSzPrS4qbIU=--NM2DXenUUCrSItNzjUpGe56ByFwH5HduGQH4UUTMpzM="
-NAME="shoes"
-PRICE=49.99
-DESCRIPTION="blue shoes"
+TOKEN="QCWV8a49nIfmcfuOpEcU4XcUaB8I42b7l1bC4+6EGEM=--5SDx1bGxQ8VqEIsZw8Q0YY3XnKQYvTrO3T6bTFupLy8="
+NAME="sweatshirt"
+PRICE=39.99
+DESCRIPTION="hoodie"
 IMG="path"
 VERB=GET
 
